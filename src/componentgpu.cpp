9b72502e1d8d0301f265bf70565c1be4a7419e4e
alakazard12 <jacobjm18@gmail.com>
2014-09-24 20:47:01 -0400

    Event system

---
+++ src/componentgpu.cpp
@@ -0,0 +1,5 @@
+#include "componentgpu.h"
+
+ComponentGPU::ComponentGPU()
+{
+}
---
+++ src/componentgpu.h
@@ -0,0 +1,10 @@
+#ifndef COMPONENTGPU_H
+#define COMPONENTGPU_H
+
+class ComponentGPU : public Component
+{
+public:
+    ComponentGPU();
+};
+
+#endif // COMPONENTGPU_H
