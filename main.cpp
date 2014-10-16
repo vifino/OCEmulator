fbd91ab75ecc96e4ecbca9053e196bbc0c5c5cd4
Jacob Manning <jacobjm18@gmail.com>
2014-10-16 17:56:33 -0400

    Updates

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <gtkmm.h>
+
+#include <boost/filesystem.hpp>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+
+#include "httprequest.h"
+#include "ocewindow.h"
+#include "computerinstance.h"
+
+extern "C"
+{
+  #include <lua.h>
+}
+
+using boost::asio::ip::tcp;
+
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "ml.altenius.ocemulator");
+    
+    OCEWindow win;
+
+    /*HttpRequest req;
+    req.get("www.google.com", "/");
+    
+    std::cout << "data: " << req.readLine() << std::endl;*/
+    
+    std::string homeDir = getenv("HOME");
+    
+    boost::filesystem::create_directories(homeDir + "/.ocemulator");
+    
+    // Download kernel.lua
+    
+    if (!boost::filesystem::exists(homeDir + "/.ocemulator/kernel.lua"))
+    {
+      HttpRequest kernelReq;
+      kernelReq.get("www.pastebin.com", "/raw.php?i=eS7N7Mvh"); // TODO: Check if this failed
+      
+      std::ofstream kernelFile;
+      kernelFile.open(homeDir + "/.ocemulator/kernel.lua");
+      kernelFile << kernelReq.readAll();
+      kernelFile.close();
+    }
+    
+    ComputerInstance comp(homeDir + "/.ocemulator", "default", win.getScreenW());
+    
+    app->run(win, argc, argv);
     return 0;
 }
